e4e0520112b76a070056698edf0c1b6111406fd0
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-04-11 19:36:55 -0400

    stub up Store exercise

--- README.md
+++ README.md
@@ -56,3 +56,28 @@ Run the code to see some account behavior happen.
 Look at the main method! It’s so long! There is so much repeated code! Can you imagine how messy it would look if you needed to make 10 deposits?
 
 Throughout this lesson, we will learn how to make methods that would make checking the balance, depositing, and withdrawing all behavior that would take only one line of code.
+
+## Store
+
+```
+public class Store {
+  // instance fields
+  String productType;
+  
+  // constructor method
+  public Store(String product) {
+    productType = product;
+  }
+  
+  // advertise method
+  
+  // main method
+  public static void main(String[] args) {
+    
+  }
+}
+```
+
+In between the constructor and the main() method, add a method called advertise() to the Store class. It should be accessible by other classes, and should have no output.
+
+You can leave the body of the method empty.
--- src/App.java
+++ src/App.java
@@ -16,5 +16,35 @@ public class App {
      * 
      * Procedural abstractions is the knowledge of what a method does, its end result, without fully knowing how it accomplishes it
      */
+
+    /*
+     * ----------------
+     * Defining Methods
+     * ----------------
+     * 
+     * If we defined a checkBalance() method for the Savings Account, it would look like:
+     */
+
+    // public void checkBalance() {
+    //   System.out.println("Hello!");
+    //   System.out.println("Your balance is " + balance);
+    // }
+
+    /*
+     * the first line, public void checkBalance(), is the method declaration
+     * 
+     * public means that other classes can access this method
+     * void means that there is no specific output from the method
+     * checkBalance() is the name of the method
+     * 
+     * Every method has its own unique method signature, which is comprised of the method's name and its parameter type
+     * In this example the method signature is checkBalance()
+     * 
+     * The two print statements are insside the body of the method, defined by the curly braces {  }
+     * 
+     * Anything we do in our main method, we can do in other methods
+     * 
+     * checkBalance() is considered an non-static method because its static signature does not include the keyword static like the main method() does
+     */
   }
 }
