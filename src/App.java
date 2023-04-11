327aa7bfdc4ac87471f18a36731c903515d96c97
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-04-11 19:20:27 -0400

    stub up SavingsAccount exercise

---
+++ src/App.java
@@ -0,0 +1,20 @@
+public class App {
+  public static void main(String[] args) {
+    /*
+     * ------------
+     * Introduction
+     * ------------
+     * 
+     * In the last lesson, we learned objects have state and behavior
+     * We give objects state via instance fields
+     * 
+     * We're going to create object behavior using methods
+     * 
+     * Methods are repeatable, modular blocks of code used to accomplish specific tasks
+     * 
+     * We can define our own methods that will take input and return the kind of output we want
+     * 
+     * Procedural abstractions is the knowledge of what a method does, its end result, without fully knowing how it accomplishes it
+     */
+  }
+}
--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -1,8 +1,3 @@
-## Java Methods
-
-## SavingsAccount
-
-```
 public class SavingsAccount {
   
   int balance;
@@ -47,12 +42,3 @@ public class SavingsAccount {
     
   }       
 }
-```
-
-We have made a SavingsAccount class without using any methods beyond main() and the constructor, SavingsAccount().
-
-Run the code to see some account behavior happen.
-
-Look at the main method! It’s so long! There is so much repeated code! Can you imagine how messy it would look if you needed to make 10 deposits?
-
-Throughout this lesson, we will learn how to make methods that would make checking the balance, depositing, and withdrawing all behavior that would take only one line of code.
